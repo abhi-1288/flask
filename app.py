002da24e6f949adc7149bc94d5f3b3f8f0bae592
Abhijeet Singh Rana <dell1558laptop@gmail.com>
2023-04-30 16:32:45 +0530

    initial

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+import subprocess
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/wifi_profiles')
+def get_wifi_profiles():
+    data = (subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n'))
+    profile = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i ]
+    results = []
+    for i in profile:
+        cmd = ['netsh', 'wlan', 'show', 'profiles', i, 'key=clear']
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
+        out, err = proc.communicate()
+        output = out.decode('utf-8').split('\n')
+        password = [b.split(":")[1][1:-1] for b in output if "Key Content" in b]
+        try:
+            results.append({"name": i, "password": password[0]})
+        except IndexError:
+            results.append({"name": i, "password": ""})
+    return jsonify(results)
+
+if __name__ == '__main__':
+    app.run(debug=False)
